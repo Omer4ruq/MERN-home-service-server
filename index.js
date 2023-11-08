81126b940ebd60d51daf1ec9d0aadd5cd12b1f41
Omer4ruq <omerfaruq4@gmail.com>
2023-11-08 17:26:51 +0600

    jwt middleware created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(cookieParser());
 
 console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.npoax.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,6 +28,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// jwt middlewears
+const logger = (req, res, next) => {
+  console.log("log info", req.method, req.url);
+  next();
+};
+
+const verifyToken = (req, res next) =>{
+  const token = req?.cookies?.token
+  // console.log('token in the middleware', token)
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded
+    next()
+  })
+  // next()
+}
 
 async function run() {
   try {
@@ -48,8 +72,9 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/booked", async (req, res) => {
+    app.get("/booked", logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
+      
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -74,7 +99,7 @@ async function run() {
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       console.log("user for token", user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN - SECRET, {
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
       res
